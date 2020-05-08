1fdb5e733317d8ebd5d5049d29afd126fa30f8f5
ETY001 <work@akawa.ink>
2020-05-08 20:40:16 +0800

    slack lib

--- bin/lib/slack.py
+++ bin/lib/slack.py
@@ -1,5 +1,6 @@
 #encoding:UTF-8
 import json, os, sys, time
+from urllib.request import Request, urlopen
 
 env_dist = os.environ
 slack_url = env_dist.get('SLACK_URL')
@@ -8,3 +9,13 @@ def send(msg):
     if slack_url == None:
         print("\n-------Has not config SLACK_URL.-------\n")
         return
+    message = {
+        "text": msg
+    }
+    req = Request(slack_url, json.dumps(message).encode('utf-8'))
+    try:
+        response = urlopen(req)
+        response.read()
+    except:
+        print('slack_msg_send_error:', sys.exc_info())
+    
