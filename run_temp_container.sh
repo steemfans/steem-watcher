4fbd262b25b5d2318bb30c261a4eaffcc63992e7
ETY001 <work@akawa.ink>
2020-07-23 20:50:33 +0900

    update temp container

--- run_temp_container.sh
+++ run_temp_container.sh
@@ -1,7 +1,7 @@
 docker run -it --rm \
     -v $(pwd)/bin:/app \
     --env-file $(pwd)/.env \
-    --network steem-watcher_steem_watcher_net \
+    --network steemwatcher_steem_watcher_net \
     ety001/steem-python:mysql \
     /bin/ash
     
